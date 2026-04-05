1c000fa02d98c7336eaf94aee357435e028ac703
dmitkov28 <mitkov.dimitar@yahoo.com>
2026-04-05 12:10:19 +0300

    add abstract storage class & factories

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from duckdb import DuckDBPyConnection
 import duckdb
 
+from storage import DuckLakeStorage
+
 
 @dataclass
 class PgConfig:
@@ -15,42 +17,20 @@ class PgConfig:
     pg_port: Optional[int] = 5432
 
 
-@dataclass
-class MinioConfig:
-    endpoint: str
-    bucket: str
-    user: str
-    password: str
-
-
 class DuckLakeClient:
     def __init__(
-        self, ddb: DuckDBPyConnection, pg_config: PgConfig, minio_config: MinioConfig
+        self, ddb: DuckDBPyConnection, pg_config: PgConfig, storage: DuckLakeStorage
     ):
         self._ddb = ddb
         self._pg = pg_config
-        self._minio = minio_config
-
-    def _create_minio_secret(self):
-        self._ddb.execute(
-            f"""
-            CREATE SECRET minio_secret (
-                TYPE S3,
-                KEY_ID '{self._minio.user}',
-                SECRET '{self._minio.password}',
-                ENDPOINT '{self._minio.endpoint}',
-                USE_SSL false,
-                URL_STYLE 'path'
-            );
-        """
-        )
+        self._storage = storage
 
     def connect(self):
-        self._create_minio_secret()
+        self._storage.create_duckdb_secret(self._ddb)
         self._ddb.execute(
             f"""
             ATTACH 'ducklake:postgres:dbname={self._pg.pg_db} host={self._pg.pg_host} user={self._pg.pg_user} password={self._pg.pg_pass}' 
-            AS ducklake (DATA_PATH 's3://{self._minio.bucket}/');
+            AS ducklake (DATA_PATH 's3://{self._storage.bucket}/');
             """
         )
 
@@ -64,7 +44,7 @@ class DuckLakeClient:
         return self._ddb.execute(command)
 
 
-def get_client() -> DuckLakeClient:
+def get_client(storage: DuckLakeStorage) -> DuckLakeClient:
     import os
 
     load_dotenv()
@@ -73,18 +53,7 @@ def get_client() -> DuckLakeClient:
     PG_PASS = os.environ["POSTGRES_PASSWORD"]
     PG_DB = os.environ["POSTGRES_DB"]
 
-    MINIO_USER = os.environ["MINIO_ROOT_USER"]
-    MINIO_PASS = os.environ["MINIO_ROOT_PASSWORD"]
-    MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost:9000")
-    MINIO_BUCKET = os.getenv("MINIO_BUCKET", "ducklake")
-
     pg = PgConfig(pg_host="localhost", pg_user=PG_USER, pg_pass=PG_PASS, pg_db=PG_DB)
-    minio = MinioConfig(
-        endpoint=MINIO_ENDPOINT,
-        bucket=MINIO_BUCKET,
-        user=MINIO_USER,
-        password=MINIO_PASS,
-    )
 
-    client = DuckLakeClient(ddb=duckdb.connect(), pg_config=pg, minio_config=minio)
+    client = DuckLakeClient(ddb=duckdb.connect(), pg_config=pg, storage=storage)
     return client
---
+++ storage.py
@@ -0,0 +1,132 @@
+from typing import Protocol
+
+import boto3
+from botocore.client import Config
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+from duckdb import DuckDBPyConnection
+
+
+class DuckLakeStorage(Protocol):
+    bucket: str
+
+    def create_bucket(self) -> None: ...
+
+    def create_duckdb_secret(self, ddb: DuckDBPyConnection) -> None: ...
+
+
+class MinioStorage(DuckLakeStorage):
+    def __init__(self, endpoint: str, bucket_name: str, user: str, password: str):
+        self._endpoint = endpoint
+        self._bucket_name = bucket_name
+        self._user = user
+        self._password = password
+
+    @property
+    def bucket(self) -> str:
+        return self._bucket_name
+
+    def create_bucket(self) -> None:
+        s3 = boto3.client(
+            "s3",
+            endpoint_url=f"http://{self._endpoint}",
+            aws_access_key_id=self._user,
+            aws_secret_access_key=self._password,
+            config=Config(signature_version="s3v4"),
+        )
+        try:
+            s3.create_bucket(Bucket=self._bucket_name)
+        except ClientError as e:
+            if e.response["Error"]["Code"] != "BucketAlreadyOwnedByYou":
+                raise
+
+    def create_duckdb_secret(self, ddb: DuckDBPyConnection) -> None:
+        ddb.execute(
+            f"""
+            CREATE SECRET s3_secret (
+                TYPE S3,
+                KEY_ID '{self._user}',
+                SECRET '{self._password}',
+                ENDPOINT '{self._endpoint}',
+                USE_SSL false,
+                URL_STYLE 'path'
+            );
+        """
+        )
+
+
+class S3Storage(DuckLakeStorage):
+    def __init__(
+        self, bucket_name: str, region: str, access_key_id: str, secret_access_key: str
+    ):
+        self._bucket_name = bucket_name
+        self._region = region
+        self._access_key_id = access_key_id
+        self._secret_access_key = secret_access_key
+
+    @property
+    def bucket(self) -> str:
+        return self._bucket_name
+
+    def create_bucket(self) -> None:
+        s3 = boto3.client("s3", region_name=self._region)
+        try:
+            s3.create_bucket(
+                Bucket=self._bucket_name,
+                CreateBucketConfiguration={"LocationConstraint": self._region},
+            )
+        except ClientError as e:
+            if e.response["Error"]["Code"] not in (
+                "BucketAlreadyOwnedByYou",
+                "BucketAlreadyExists",
+            ):
+                raise
+
+    def create_duckdb_secret(self, ddb: DuckDBPyConnection) -> None:
+        ddb.execute(
+            f"""
+            CREATE SECRET s3_secret (
+                TYPE S3,
+                KEY_ID '{self._access_key_id}',
+                SECRET '{self._secret_access_key}',
+                REGION '{self._region}',
+                USE_SSL true
+            );
+        """
+        )
+
+
+def get_minio_storage() -> DuckLakeStorage:
+    import os
+
+    load_dotenv()
+    MINIO_USER = os.environ["MINIO_ROOT_USER"]
+    MINIO_PASS = os.environ["MINIO_ROOT_PASSWORD"]
+    MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
+    MINIO_BUCKET = os.getenv("MINIO_BUCKET", "ducklake")
+
+    storage = MinioStorage(
+        endpoint=MINIO_ENDPOINT,
+        bucket_name=MINIO_BUCKET,
+        user=MINIO_USER,
+        password=MINIO_PASS,
+    )
+    return storage
+
+
+def get_s3_storage() -> DuckLakeStorage:
+    import os
+
+    load_dotenv()
+    AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    AWS_REGION = os.getenv("AWS_REGION", "eu-central-1")
+    S3_BUCKET = os.environ["S3_BUCKET"]
+
+    s3_storage = S3Storage(
+        region=AWS_REGION,
+        access_key_id=AWS_ACCESS_KEY_ID,
+        secret_access_key=AWS_SECRET_ACCESS_KEY,
+        bucket_name=S3_BUCKET,
+    )
+    return s3_storage
